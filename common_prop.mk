3ba444a4aaf530d6df15062e612e6496c46ebe17
PradyX <pradyumnbhushan@gmail.com>
2020-04-15 19:18:39 +0530

    sdm660-common: enable package blacklist for aux lens

--- common_prop.mk
+++ common_prop.mk
@@ -122,6 +122,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.eis.enable=1 \
     persist.vendor.camera.exif.make=Xiaomi \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam \
+    vendor.camera.aux.packagelist=com.android.camera,org.codeaurora.snapcam,com.android.GoogleCamera \
     vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord
 
 # Codec2 switch
